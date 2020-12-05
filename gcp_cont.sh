fc63f9d78f0d37152a1f855e67f50fdaf5bf91e2
Sammy970 <70690987+Sammy970@users.noreply.github.com>
2020-12-05 14:08:13 +0530

    Update gcp_cont.sh

--- gcp_cont.sh
+++ gcp_cont.sh
@@ -4,7 +4,7 @@ echo "  So to create a snapshot of your vm instance;"
 echo "  Give me the name of your instance;"
 read inst
 echo "  Give me the name of your zone in which your instance is !!;"
-echo zone
+read zone
 cd 
 gcloud compute disks snapshot $inst --snapshot-names s1 --zone $zone
 echo "   Now, lets create an image from the snapshot;"
